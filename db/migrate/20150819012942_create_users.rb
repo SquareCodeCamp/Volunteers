b11c29e143ab2e4b4d562c67aa2a9919c866be55
Kathy Li <li.kathy12@gmail.com>
2015-08-19 22:45:34 -0700

    Added encrypted password + verification for users

--- db/migrate/20150819012942_create_users.rb
+++ db/migrate/20150819012942_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name, {null: false}
+      t.string :name, null: false
+      t.string :encrypted_password, null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,8 @@ ActiveRecord::Schema.define(version: 20150820012925) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name",               null: false
+    t.string "encrypted_password", null: false
   end
 
 end
