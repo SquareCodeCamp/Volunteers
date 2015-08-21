ca60394232de9b095354a8f409235c944f45543b
Kathy Li <li.kathy12@gmail.com>
2015-08-20 20:49:21 -0700

    Added users controller and new user view

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
      resources :events, only: [:index, :create, :destroy, :update]
+     resources :users
 
   # Example resource route with options:
   #   resources :products do
