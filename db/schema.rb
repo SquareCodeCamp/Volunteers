b6d962fed85d106e545522e32632773c02ff0535
Kathy Li <li.kathy12@gmail.com>
2015-08-20 17:57:59 -0700

    Added email to users, created users controller

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150820012925) do
 
   create_table "users", force: :cascade do |t|
     t.string "name",               null: false
+    t.string "email",              null: false
     t.string "encrypted_password", null: false
   end
 
